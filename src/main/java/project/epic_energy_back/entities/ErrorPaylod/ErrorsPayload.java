4a6686703db5e84d59e5b1e600ae4a3d555975ce
fdinocera <38116029+fdinocera@users.noreply.github.com>
2024-06-21 12:40:06 +0200

    Add files via upload

--- src/main/java/project/epic_energy_back/entities/ErrorPaylod/ErrorsPayload.java
+++ src/main/java/project/epic_energy_back/entities/ErrorPaylod/ErrorsPayload.java
@@ -1,12 +1,12 @@
-package project.epic_energy_back.entities.ErrorPaylod;
-
-import java.util.Date;
-
-public record ErrorsPayload(
-        String message,
-        Date timestamp) {
-    public ErrorsPayload(String message, Date timestamp) {
-        this.message = message;
-        this.timestamp = timestamp;
-    }
-}
+//package project.epic_energy_back.entities.ErrorPaylod;
+//
+//import java.util.Date;
+//
+//public record ErrorsPayload(
+//        String message,
+//        Date timestamp) {
+//    public ErrorsPayload(String message, Date timestamp) {
+//        this.message = message;
+//        this.timestamp = timestamp;
+//    }
+//}
